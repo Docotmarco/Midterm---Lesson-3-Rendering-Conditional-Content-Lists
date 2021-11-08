edc41a0b85c7af071e27b9c477aaadc539ee8175
Docotmarco <marcodocot1234567@gmail.com>
2021-11-08 23:24:46 +0800

    adding functiion in togller button

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ methods:{
     add_goal(){
         this.goals.push(this.entergoalvalue)
     },
+    tog_btn(){
+        this.toggle_btn = !this.toggle_btn
+  
+        if(this.toggle_btn === true){
+          this.toggler = 'Show List'
+        }else{
+          this.toggler = 'Hide List'
+        }
+      }
 }
 });
   
